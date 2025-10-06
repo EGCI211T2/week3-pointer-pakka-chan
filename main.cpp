86f7ebd464ac8ac24547bb3868dd5db1a59a4400
pakka-chan <pakkanat.apr@student.mahidol.edu>
2025-10-06 08:17:07 +0000

    Chan

--- main.cpp
+++ main.cpp
@@ -6,15 +6,40 @@ using namespace std;
 
 int main(int argc,char *argv[]){
   int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
+int n =argc-1;
+
+  pa=new int[n];
+
+  for(i=0;i<n;i++){
+
+   // cout<<"Input no."<<i<<":";
+
+    //cin>> *pa;
+
+    *pa=atoi(argv[i+1]);
+
+  }
+  
+  pa-=n;
+
 cout<<"Original: ";
 for (i=0; i<SIZE-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
+pb=pa;
+pa -= (n-1);
 
+for (i=0; i<SIZE/2; i++)
+{
+   temp = *pa;  
+   *pa = *pb;  
+   *pb = temp;
+   pa++;  pb--;
+}
+pa -= (n/2);
+/*
 pa = &a[0];  
 pb = &a[SIZE-1];
 for (i=0; i<SIZE/2; i++)
@@ -25,14 +50,15 @@ for (i=0; i<SIZE/2; i++)
    pa++;  pb--;
 }
 pa=&a[0];
-pb-=SIZE/2;
+pb-=SIZE/2;*/
 cout<<"Reversed: ";
 for (i=0; i<SIZE-1; i++ ,pa++){
    cout<<setw(3)<<*pa ;//<<endl;
   //cout<<pa<<endl;
 }
 cout<<setw(3)<<*pa<<endl;
-
+pa-=n-1;
+delete []pa;
 
 return 0;
 }
